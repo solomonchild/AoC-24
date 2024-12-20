01a65597165b8664eae2d3f0de1b1bca98711819
Vladislavs Burakovs <vburakovs@evolution.com>
2024-12-20 21:44:17 +0200

    Fixed wrong template argument

--- src/day21.cpp
+++ src/day21.cpp
@@ -1,13 +1,13 @@
 #include "common.h"
 
 template <>
-std::string DaySolver<20>::part1()
+std::string DaySolver<21>::part1()
 {
 	return "";
 }
 
 template <>
-std::string DaySolver<20>::part2()
+std::string DaySolver<21>::part2()
 {
 	return "";
 }
